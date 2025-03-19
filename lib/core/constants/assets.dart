39bf95584120226e24842d90f04c233e2b5cb991
3issa <mu7ammad.3issa@gmail.com>
2025-03-19 04:29:32 +0200

    splash for android 12+

--- lib/core/constants/assets.dart
+++ lib/core/constants/assets.dart
@@ -8,6 +8,8 @@ class Assets {
 
   static const String iconsSplashArrow = "assets/icons/splash_arrow.svg";
 
+  static const String imagesAndroid12Splash = "assets/images/android_12_splash.png";
+
   static const String imagesLogo = "assets/images/logo.png";
 
   static const String imagesSplashBackground = "assets/images/splash_background.png";
